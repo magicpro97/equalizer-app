79c5fca09729de562384ef8ff7fd3ac5fa0f0f75
Linh Ngo <thlinh.ngo@gmail.com>
2024-10-28 20:43:05 +0700

    Update UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,29 @@ st.write(
     "This is a simple equalizer app that allows you to adjust the levels of different frequency bands."
 )
 
-st_audio = st.empty()  # Placeholder for audio playback
 recorder = None  # Placeholder for the recorder object
 # Add buttons to apply and reset the equalizer settings in the same row
 col0, col1, col2 = st.columns([5, 3, 2])
 
 def audio_callback(audio_bytes):
-    st_audio.audio(audio_bytes, format="audio/wav")
+    st.audio(audio_bytes, format="audio/wav")
 
 with col0:
     col0.subheader("Audio Source")
     source = st.selectbox("Select Audio Source", ["Microphone", "File Upload"], placeholder="Select an audio source")
+
+    col0_1, col0_2 = col0.columns([1, 5], vertical_alignment="center")
     if source == "File Upload":
         uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
         if uploaded_file:
             st.session_state.my_recorder_output = uploaded_file
             audio_callback(uploaded_file.read())
     else:
-        st.write("Using microphone input.")
-        recorder = mic_recorder(start_prompt="🔴", stop_prompt="⏹️", key='recorder')
-        if recorder:
-            audio_callback(recorder['bytes'])
+        with col0_1:
+            recorder = mic_recorder(start_prompt="🔴", stop_prompt="⏹️", key='recorder')
+        with col0_2:
+            if recorder:
+                audio_callback(recorder['bytes'])
 
 with col1:
     col1.subheader("Equalizer Settings")
