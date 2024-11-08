367b3493e4e0aa39c1f6fff31a6f4ceec066a587
Linh Ngo <thlinh.ngo@gmail.com>
2024-11-08 09:52:18 +0700

    fix: visualize audio

--- fir.py
+++ fir.py
@@ -11,13 +11,15 @@ def apply_filters(audio_data, bass_gain, mid_gain, treble_gain):
     filtered_audio = bass_filtered + mid_filtered + treble_filtered
     return filtered_audio, bass_filtered, mid_filtered, treble_filtered
 
-def apply_bandpass_filter(data, lowcut, highcut, gain, fs=SAMPLE_RATE, numtaps=101):
+def apply_bandpass_filter(data, lowcut, highcut, gain, fs=SAMPLE_RATE, numtaps=101, apply_gain=True):
     nyquist = 0.5 * fs
     low = lowcut / nyquist
     high = highcut / nyquist
     taps = firwin(numtaps, [low, high], pass_zero=False)
     filtered_data = lfilter(taps, 1.0, data)
-    return filtered_data * gain
+    if apply_gain:
+        filtered_data *= gain
+    return filtered_data
 
 def speed_up_audio(audio_data, factor):
     indices = np.round(np.arange(0, len(audio_data), factor)).astype(int)
